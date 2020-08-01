fd6e9c979dcfc853d18f802006fba3e448dfdc76
SinkLineP <popovartem505@gmail.com>
2020-08-01 13:17:29 +0300

    add js files

---
+++ Site Web/javascript/main.js
@@ -0,0 +1,9 @@
+$('.home-slider').owlCarousel({
+    animateOut: 'fadeOut',
+    items: 1,
+    loop: true,
+    dots: false,
+    autoplayHoverPause: false,
+    autoplay: true,
+    smartspeed: 1000,
+})
